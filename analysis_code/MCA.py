652ae9413f15294081fb03c35942929a1be2397f
Yuliia Hrabar <yuliia@tau.nuclear.lu.se>
2020-08-11 11:05:31 +0200

    Changed Spectrum.y to Spectrum.counts in MCA structure

--- analysis_code/MCA.py
+++ analysis_code/MCA.py
@@ -12,16 +12,16 @@ class Spectrum:
         self.bin_edges = np.array(np.zeros(1))    ## creates a new empty array; will store edges of our bins
         self.bin_centers = np.array(np.zeros(1))
         self.energy = np.array(np.zeros(1))
-        self.y = np.array(np.zeros(1))    ## creates a new empty array; will later store our y values
+        self.counts = np.array(np.zeros(1))    ## creates a new empty array; will later store our y values
         self.name = os.path.splitext(     ## a more descriptive name, can be used e.g. in legends
                 os.path.basename(filename))[0] ## init with file name without extension
         self.duration = 0
     def subtract(self, m):
-        self.y = self.y - m.y
+        self.counts = self.counts - m.counts
         ## these are spectra: cannot have counts below 0, so remove these here and set them to 0 instead:
-        self.y[self.y < 0] = 0;
+        self.counts[self.counts < 0] = 0;
     def scale(self, scale):
-        self.y *= scale
+        self.counts *= scale
     def calibrate(self, slope, intercept):
         self.energy = self.bin_centers*slope + intercept
     def calculate_bin_centers(self):
@@ -54,12 +54,12 @@ def load_spectrum(filename):
             ## TODO: make sure that the file does not end before we have parsed the header!
             log.debug("Done with header parsing")
             nchannel = int(interval[1]-interval[0])+1
-            m.y = np.array(np.zeros(nchannel))
+            m.counts = np.array(np.zeros(nchannel))
             ## continue, now reading data
             for idx, row in enumerate(reader):
                 if idx >= nchannel:
                     break
-                m.y[idx] = int(row[0])
+                m.counts[idx] = int(row[0])
             m.bin_edges = np.arange(interval[0], interval[1]+2,1)
             m.calculate_bin_centers()
             log.debug("Loaded all data from file")
@@ -72,16 +72,16 @@ def load_spectrum(filename):
 def load_calibrated_spectrum(filename):
     log = logging.getLogger('gammalab_analysis') ## set up logging
     m = Spectrum(filename) ## create a new Spectrum measurement object; this is what we return in the end
-    m.x = np.zeros(8192)
-    m.y = np.zeros(8192)
+    m.energy = np.zeros(8192)
+    m.counts = np.zeros(8192)
     log.info("Reading calibrated data from file '" + filename + "'")
     try:
         with open(filename) as f: #the with keyword handles the opening (__enter__ method) and closing (__exit__ method) of the file automatically
             reader = csv.reader(f)
             for idx, row in enumerate(reader):
-                channel, energy = row[0].split() 
-                m.y[idx] = int(channel)
-                m.x[idx] = float(energy)
+                counts, energy = row[0].split() 
+                m.count[idx] = int(counts)
+                m.energy[idx] = float(energy)
     except IOError:
         log.error("Could not find the file '"+str(filename)+"'")
         sys.exit(-1)
